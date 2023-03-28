ed02467d2ac0f2e1354c5c6aa52d41d94c47d992
Svetlana <pidgin66@gmail.com>
2023-03-28 19:57:40 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from "commander/esm.mjs";
-import { readFileSync } from 'node:fs';
-import compare from 'src/compare.js'
+import genDiff from 'src/genDiff.js'
 
 program
 		.description('Compares two configuration files and shows a difference.')
 		.version('0.1.0')
-		.arguments('<filepath1> <filepath2>')
+		.arguments('<filepath1>')
+		.arguments('<filepath2>')
 		.option('-f, --format <type>', 'output format')
-		.action()
+		.action((filepath1, filepath2) =>{
+				return console.log(genDiff(filepath1, filepath2));
+		})
 
 program.parse();
