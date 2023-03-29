1ddcb5bd68d5dc39fa98648710f8bcc1b88d9699
Svetlana <pidgin66@gmail.com>
2023-03-29 18:48:38 +0300

    reinstall eslint

--- src/genDiff.js
+++ src/genDiff.js
@@ -5,7 +5,14 @@ const genDiff = (filepath1, filepath2) => {
   const obj1 = parse(filepath1);
   const obj2 = parse(filepath2);
 
+
   return [obj1, obj2];
 };
+
+
+const file1 = '../__fixtures__/file1.json';
+const file2 = '../__fixtures__/file2.json';
+
+console.log(genDiff(file1, file2));
 
 export default genDiff;
--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs';
+//import { cwd } from 'node:process';
 import path from 'path';
 
 const parse = (file) => JSON.parse(readFileSync(path.resolve(file), 'utf-8'));
