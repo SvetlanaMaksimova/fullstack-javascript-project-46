edd78385d1e438d9af2beae44b326d678a9e579c
Svetlana <pidgin66@gmail.com>
2023-04-15 13:10:08 +0300

    add new test for trees

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,16 @@
 // import { fileURLToPath } from 'url';
 // import { dirname } from 'path';
+import fs from 'fs';
 import genDiff from '../src/genDiff.js';
 
-const filepath1 = '__fixtures__/file1.json';
-const filepath2 = '__fixtures__/file2.json';
+const jsonFilepath1 = '__fixtures__/file1.json';
+const jsonFilepath2 = '__fixtures__/file2.json';
 
 const filepathYaml1 = '__fixtures__/file1.yaml';
 const filepathYaml2 = '__fixtures__/file2.yaml';
 
-//
+const stylishExample = fs.readFileSync('./__fixtures__/stylish', 'utf-8');
+
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
 //
@@ -20,27 +22,10 @@ const filepathYaml2 = '__fixtures__/file2.yaml';
 // const yamlFilepath1 = getFixturePath('file1.yaml');
 // const yamlFilepath2 = getFixturePath('file2.yaml');
 
-const resultDiff = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-
 test('testing diff of two json file', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(resultDiff);
+  expect(genDiff(jsonFilepath1, jsonFilepath2)).toEqual(stylishExample);
 });
 
 test('testing diff of two yaml file', () => {
-  expect(genDiff(filepathYaml1, filepathYaml2)).toEqual(resultDiff);
+  expect(genDiff(filepathYaml1, filepathYaml2)).toEqual(stylishExample);
 });
-
-// test('testing diff of two json file', () => {
-//   expect(genDiff(jsonFilepath1, jsonFilepath2)).toEqual(resultDiff);
-// });
-//
-// test('testing diff of two yaml file', () => {
-//   expect(genDiff(yamlFilepath1, yamlFilepath2)).toEqual(resultDiff);
-// });
