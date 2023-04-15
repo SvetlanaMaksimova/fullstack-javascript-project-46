2818fade2f2d88d113af3a7e464a46c2538971eb
Svetlana <pidgin66@gmail.com>
2023-04-15 13:11:40 +0300

    fix format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .version('0.1.0')
   .arguments('<filepath1>')
   .arguments('<filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, type) => console.log(genDiff(filepath1, filepath2, type)));
 
 program.parse();
