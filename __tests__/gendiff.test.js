1f861c8aa51e6237a360cbe1dc6c00dc8b98d7e5
Svetlana <pidgin66@gmail.com>
2023-04-18 13:13:32 +0300

    delete comments

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,3 @@
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/genDiff.js';
 
@@ -12,17 +10,6 @@ const filepathYaml2 = '__fixtures__/file2.yaml';
 const stylishExample = fs.readFileSync('./__fixtures__/stylish', 'utf-8');
 const plainExample = fs.readFileSync('./__fixtures__/plain', 'utf-8');
 const jsonExample = fs.readFileSync('./__fixtures__/json', 'utf-8');
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-//
-// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-//
-// const jsonFilepath1 = getFixturePath('file1.json');
-// const jsonFilepath2 = getFixturePath('file2.json');
-//
-// const yamlFilepath1 = getFixturePath('file1.yaml');
-// const yamlFilepath2 = getFixturePath('file2.yaml');
 
 test('stylish testing', () => {
   expect(genDiff(jsonFilepath1, jsonFilepath2)).toEqual(stylishExample);
